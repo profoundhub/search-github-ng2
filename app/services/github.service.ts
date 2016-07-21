f51206d32ec970c064c161271bff2e84fbd05a82
Daniel Lim <hbt@markhamboards.com>
2016-07-21 12:00:10 -0400

    Update 2

---
+++ app/services/github.service.ts
@@ -0,0 +1,24 @@
+import {Injectable} from '@angular/core';
+import {Http, Headers} from '@angular/http';
+import 'rxjs/add/operator/map';
+
+@Injectable()
+export class GithubService {
+  private username; // Github Username
+
+  constructor(private _http:Http){
+    this.username = 'profoundhub';
+  }
+
+  // Get User Profile Info
+  getUser(){
+    return this._http.get('https://api.github.com/users/'+this.username)
+      .map(res => res.json());
+  }
+
+  // Get User Repos
+  getRepos(){
+    return this._http.get('https://api.github.com/users/'+this.username+'/repos')
+      .map(res => res.json());
+  }
+}
